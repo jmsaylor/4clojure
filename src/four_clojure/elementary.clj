f59cb32f97fb507912a1eb46ece220203f11abb4
John Saylor <johnmsaylor@gmail.com>
2022-05-05 19:56:38 -0400

    up to 10 Maps

--- src/four_clojure/elementary.clj
+++ src/four_clojure/elementary.clj
@@ -12,6 +12,12 @@
 
 (defn list-equality [] (= (list :a :b :c) '(:a :b :c)))
 
-(defn get-5 [x] (conj '("Joe" 5) {:a 'b} true x))
+(defn list-add [x] (conj '("Joe" 5) {:a 'b} true x))
 
 (defn vector-equality [] (= [:a :b :c] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c)))
+
+(defn set-union [] (= #{:a :b :c} (clojure.set/union #{:a :b} #{:b :c})))
+
+(defn set-add [] (= #{1 2 3 4 5} (conj #{1 4 3 5} 2)))
+
+(defn map-get-key [] (:last {:first "John" :last "Saylor"}))
