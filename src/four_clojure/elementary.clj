9733a58b503c23728093c838b0e0f71183d63081
John Saylor <johnmsaylor@gmail.com>
2022-05-05 19:28:11 -0400

    up to #5

--- src/four_clojure/elementary.clj
+++ src/four_clojure/elementary.clj
@@ -4,4 +4,14 @@
 
 (defn nt [] n)
 
+(defn is-it-true [f] (f))
+
 (defn simple-math [] (= (- 10 (* 2 3)) 4))
+
+(defn capitalize [string] (.toUpperCase string))
+
+(defn list-equality [] (= (list :a :b :c) '(:a :b :c)))
+
+(defn get-5 [x] (conj '("Joe" 5) {:a 'b} true x))
+
+(defn vector-equality [] (= [:a :b :c] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c)))
