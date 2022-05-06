98c66453f805faff9bd816b095aad8e96e550ba8
John Saylor <johnmsaylor@gmail.com>
2022-05-06 07:53:02 -0400

    sequences

--- src/four_clojure/elementary.clj
+++ src/four_clojure/elementary.clj
@@ -21,3 +21,13 @@
 (defn set-add [] (= #{1 2 3 4 5} (conj #{1 4 3 5} 2)))
 
 (defn map-get-key [] (:last {:first "John" :last "Saylor"}))
+
+(defn one-two-three [] (conj {:a 1} {:b 2} {:c 3}))
+
+(defn id [name] (map (fn [name] {:name name :id (rand-int 100000000)}) [name]))
+
+(defn turtle-sequence [] (first (conj [:haire] :turtle)))
+
+(defn sequences [] (= (first '(1 2 3)) (second [2 1 3]) (last (list 3 2 1))))
+
+(defn the-rest [] (rest '(0 1 2 3)))
