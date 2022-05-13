c21d1b41a7179767729cdd3ce8b8f54063ebc7d3
John Saylor <johnmsaylor@gmail.com>
2022-05-12 22:30:14 -0400

    associative destructuring

--- src/four_clojure/easy.clj
+++ src/four_clojure/easy.clj
@@ -9,4 +9,10 @@
 (defn penultimate-of-list [] (#(let [v (vec %)]
                                 (get v (- (count v) 2))) '(1 2 3 4 5)))
 
+(def cars [{:name 'Porsche :year 1995} {:name 'Cadillac :year 1970} {:name 'Tesla :year 2021}])
 
+(defn group-new [cars] (let [{new-cars 'true} (group-by #(< 2020 (:year %)) cars)] new-cars))
+
+(defn get-cars [] cars)
+
+(defn test-let [] (let [tester true] tester))
