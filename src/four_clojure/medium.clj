a2f092c086bf3f7deae351023023b1e868611527
John Saylor <johnmsaylor@gmail.com>
2022-06-21 06:46:39 -0400

    reverse interleave

--- src/four_clojure/medium.clj
+++ src/four_clojure/medium.clj
@@ -2,9 +2,9 @@
 
 (def test-n 'test)
 
-(defn rev-c [coll n]
+(defn reverse-interleave [coll n]
   (reduce
    (fn [acc [index value]] (let [group (mod index n)]
-     (assoc acc group (cons value (get acc group)))))
+     (assoc acc group (conj (get acc group) value))))
    (vec (repeat n []))
-   (keep-indexed (fn [index value] [(inc index) value]) coll)))
+   (keep-indexed (fn [index value] [index value]) coll)))
